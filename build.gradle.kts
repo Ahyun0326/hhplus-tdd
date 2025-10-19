f482ec0a3c016f5015560834b264111409f3990a
Yeseo Kim <chinkl850@gmail.com>
2025-10-19 16:48:31 +0900

    build: kotest, mockk 의존성 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,6 +24,9 @@ dependencies {
     implementation(libs.spring.boot.starter.web)
     annotationProcessor(libs.spring.boot.configuration.processor)
     testImplementation(libs.spring.boot.starter.test)
+    testImplementation("io.kotest:kotest-runner-junit5:5.9.1")
+    testImplementation("io.kotest:kotest-assertions-core:5.9.1")
+    testImplementation("io.mockk:mockk:1.14.6")
 }
 
 // about source and compilation
